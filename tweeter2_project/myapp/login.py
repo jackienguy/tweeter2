8fe4d1849e8c110fe1f11b959d6e184fb90db57b
Your Name <you@example.com>
2021-10-14 15:26:11 -0600

    updates to user endpoints

--- tweeter2_project/myapp/login.py
+++ tweeter2_project/myapp/login.py
@@ -5,8 +5,6 @@ import json
 import re
 from myapp import app
 
-
-
 # POST to log in user with username and password combo
 def dbConnect():
     conn = None
@@ -31,23 +29,48 @@ def dbConnect():
     
     return (conn, cursor)
 
-# @app.route('/login', methods=['DELETE'])
-# def login():
-#     if (request.method == 'POST'):
-#         conn = None
-#         cursor = None
-#         username = request.json.get('username')
-#         password = request.json.get('password')
+@app.route('/api/login', methods=['POST','DELETE'])
+def login(username, password):
+    if (request.method == 'POST'):
+        conn = None
+        cursor = None
+        username = request.json.get('username')
+        password = request.json.get('password')
 
-#        try:
-#            (conn, cursor) = dbConnect()
-#             cursor.execute("SELECT * FROM user WHERE username=?",[username,])
+        try:
+            (conn, cursor) = dbConnect()
+            cursor.execute("SELECT * FROM user WHERE username=?",[username,])
+            result = cursor.fetchall()
+        
+            return Response(json.dumps(result, default=str),
+                                mimetype="application/json",
+                                status=200)
+                   
+        except mariadb.DataError:
+            print("something went wrong with your data")
+        except mariadb.OperationalError:
+            print("opertational error on the connection")
+        except mariadb.ProgrammingError:
+            print("apparently, you don't know how to code")
+        except mariadb.IntegrityError:
+            print("Error with DB integrity. most likelu constraint failure")
+        except:
+            print("Something went wrong")
 
+        finally:
+            if (cursor != None):
+                cursor.close()
+            if (conn != None):
+                conn.rollback()
+                conn.close()
+            else:
+                print("Failed to read data")
 
 def delete_login_session():
     if (request.method == 'DELETE'):
         conn = None
         cursor = None
+        login_token = request.json.get('')
 
         try:
             (conn, cursor) = dbConnect()
--- tweeter2_project/myapp/userEndpoint.py
+++ tweeter2_project/myapp/userEndpoint.py
@@ -27,7 +27,7 @@ def dbConnect():
     
     return (conn, cursor)
 
-@app.route('/user', methods=['GET', 'POST'])
+@app.route('/api/user', methods=['GET', 'POST', 'PATCH', 'DELETE'])
 def find_user():
     if (request.method == 'GET'):
         conn = None
@@ -39,10 +39,6 @@ def find_user():
             if id:
                 cursor.execute("SELECT * FROM user WHERE id=?", [id,])
                 result = cursor.fetchall()
-            
-                cursor.close()
-                conn.close()
-            
                 return Response(json.dumps(result, default=str),
                                 mimetype="application/json",
                                 status=200)
@@ -68,6 +64,8 @@ def find_user():
                 conn.close()
             else:
                 print("Failed to read data")
+    else:
+        return('Could not load info')
 
 def signUp():
     if (request.method == 'POST'):
@@ -78,16 +76,19 @@ def signUp():
         bio = request.json.get("bio")
         email = request.json.get("email")
         birthdate = request.json.get("birthdate")
-        print(username,password,bio,email,birthdate)
 
         try:
             (conn, cursor) = dbConnect()
             cursor.execute("INSERT INTO user(username, password, bio, email, birthdate) VALUES(?,?,?,?,?)", [username, password, bio, email, birthdate])
             conn.commit()
-           
-            print('User created')
-            cursor.close()
-            return Response(json.dumps(username, password, bio, email, birthdate),
+            resp = {
+                "username": username,
+                "password": password,
+                "bio": bio,
+                "email": email,
+                "birthdate": birthdate
+            }
+            return Response(json.dumps(resp),
                         mimetype="application/json",
                         status=200)
         
@@ -97,6 +98,8 @@ def signUp():
             #     print('valid email')
             # else:
             #     return('invalid email')
+        except ValueError as error:
+            print("Error" +str(error))
         except mariadb.DataError:
             print("something went wrong with your data")
         except mariadb.OperationalError:
@@ -116,82 +119,99 @@ def signUp():
                 conn.close()
             else:
                 print("Failed to read data")
+    else:
+        return('Sign up failed')
 
-# def edit_user():
-#     if (request.method == 'PATCH'):
-#         username = request.json.get("username")
-#         password = request.json.get("password")
-#         bio = request.json.get("bio")
-#         email = request.json.get("email")
-#         birthdate = request.json.get("birthdate")
-#         conn = None
-#         cursor = None
+def edit_user():
+    if (request.method == 'PATCH'):
+        conn = None
+        cursor = None
+        login_token = request.json.get("loginToken")
+        username = request.json.get("username")
+        password = request.json.get("password")
+        bio = request.json.get("bio")
+        email = request.json.get("email")
+        birthdate = request.json.get("birthdate")
+        
+        try:
+            (conn, cursor) = dbConnect()
+            cursor.execute("UPDATE user SET username=?, bio=?, password=?, email=?, birthdate=? WHERE id=?", [id, username, password, bio, email, birthdate])
+            conn.commit()
+            resp = {
+                "loginToken": login_token,
+                "username": username,
+                "password": password,
+                "bio": bio,
+                "email": email,
+                "birthdate": birthdate
+            }
+            return Response(json.dumps(resp),
+                        mimetype="application/json",
+                        status=200)
+        except ValueError as error:
+            print("Error" +str(error))
+        except mariadb.DataError:
+            print("something went wrong with your data")
+        except mariadb.OperationalError:
+            print("opertational error on the connection")
+        except mariadb.ProgrammingError:
+            print("apparently, you don't know how to code")
+        except mariadb.IntegrityError:
+            print("Error with DB integrity. most likelu constraint failure")
+        except:
+            print("Something went wrong")
 
-#         try:
-#             (conn, cursor) = dbConnect()
-#             cursor.execute("UPDATE user SET username=?, bio=?, password=?, email=?, birthdate=? WHERE id=?", [id, username, password, bio, email, birthdate])
-#             conn.commit()
-#             print('updated user info')
-#             cursor.close()
-#             return Response(json.dumps(username, password, bio, email, _birthdate),
-#                         mimetype="application/json",
-#                         status=200)
+        finally:
+            if (cursor != None):
+                cursor.close()
+            if (conn != None):
+                conn.rollback()
+                conn.close()
+            else:
+                print("Failed to read data")
+    else: 
+        return("Could not update user info")
 
-#         except mariadb.DataError:
-#             print("something went wrong with your data")
-#         except mariadb.OperationalError:
-#             print("opertational error on the connection")
-#         except mariadb.ProgrammingError:
-#             print("apparently, you don't know how to code")
-#         except mariadb.IntegrityError:
-#             print("Error with DB integrity. most likelu constraint failure")
-#         except:
-#             print("Something went wrong")
+def delete_user(id):
+    if (request.method == 'DELETE'):
+        conn = None
+        cursor = None
+        login_token = request.json.get("loginToken")
+        password = request.json.get('password')
 
-#         finally:
-#             if (cursor != None):
-#                 cursor.close()
-#             if (conn != None):
-#                 conn.rollback()
-#                 conn.close()
-#             else:
-#                 print("Failed to read data")
-
-# def delete_user():
-#     if (request.method == 'DELETE'):
-#         conn = None
-#         cursor = None
-#         login_token = request.json.get("loginToken")
-#         password = request.json.get('password')
-
-#         try:
-#             (conn, cursor) = dbConnect()
-#             cursor.execute("DELETE FROM user WHERE id=?",[id,])
-#             conn.commit()
-#             cursor.close()
-#             return Response(json.dumps('User deleted'),
-#                         mimetype="application/json",
-#                         status=200)
+        try:
+            (conn, cursor) = dbConnect()
+            cursor.execute("DELETE FROM user WHERE id=?",[id,])
+            conn.commit()
+            resp = {
+                "loginToken": login_token,
+                "password": password
+            }
+            return Response(json.dumps(resp),
+                        mimetype="application/json",
+                        status=200)
     
-#         except mariadb.DataError:
-#             print("something went wrong with your data")
-#         except mariadb.OperationalError:
-#             print("opertational error on the connection")
-#         except mariadb.ProgrammingError:
-#             print("apparently, you don't know how to code")
-#         except mariadb.IntegrityError:
-#             print("Error with DB integrity. most likelu constraint failure")
-#         except:
-#             print("Something went wrong")
+        except mariadb.DataError:
+            print("something went wrong with your data")
+        except mariadb.OperationalError:
+            print("opertational error on the connection")
+        except mariadb.ProgrammingError:
+            print("apparently, you don't know how to code")
+        except mariadb.IntegrityError:
+            print("Error with DB integrity. most likelu constraint failure")
+        except:
+            print("Something went wrong")
 
-#         finally:
-#             if (cursor != None):
-#                 cursor.close()
-#             if (conn != None):
-#                 conn.rollback()
-#                 conn.close()
-#             else:
-#                 print("Failed to read data")
+        finally:
+            if (cursor != None):
+                cursor.close()
+            if (conn != None):
+                conn.rollback()
+                conn.close()
+            else:
+                print("Failed to read data")
+    else:
+        return("Failed to delete user")
 
    
 
