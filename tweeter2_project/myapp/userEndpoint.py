00b41763bb91682cd3032a7e3f0efbda99fdf1c6
Your Name <you@example.com>
2021-10-14 20:41:41 -0600

    removed function for each request method and moved all under user()

--- tweeter2_project/myapp/userEndpoint.py
+++ tweeter2_project/myapp/userEndpoint.py
@@ -28,63 +28,9 @@ def dbConnect():
     
     return (conn, cursor)
 
-@app.route('/api/user', methods=['GET', 'POST', 'PATCH', 'DELETE'])
-def createUser():
-    if (request.method == 'POST'):
-        conn = None
-        cursor = None
-        username = request.json.get("username")
-        password = request.json.get("password")
-        bio = request.json.get("bio")
-        email = request.json.get("email")
-        birthdate = request.json.get("birthdate")
+@app.route('/api/user', methods=['POST','GET', 'PATCH', 'DELETE'])
 
-        try:
-            (conn, cursor) = dbConnect()
-            cursor.execute("INSERT INTO user(username, password, bio, email, birthdate) VALUES(?,?,?,?,?)", [username, password, bio, email, birthdate])
-            conn.commit()
-            login_token = secrets.token_urlsafe(20)
-            cursor.execute("INSERT INTO user_session(user_id, loginToken) VALUES=?,?",[id, login_token])
-            conn.commit()
-            newUser = {
-                "userId": id,
-                "username": username,
-                "password": password,
-                "bio": bio,
-                "email": email,
-                "birthdate": birthdate,
-                "loginToken": login_token
-            }
-            return Response(json.dumps(newUser),
-                        mimetype="application/json",
-                        status=200)
-        except ValueError as error:
-            print("Error" +str(error))
-        except mariadb.DataError:
-            print("something went wrong with your data")
-        except mariadb.OperationalError:
-            print("opertational error on the connection")
-        except mariadb.ProgrammingError:
-            print("apparently, you don't know how to code")
-        except mariadb.IntegrityError:
-            print("Error with DB integrity. most likelu constraint failure")
-        except:
-            print("Something went wrong")
-
-        finally:
-            if (cursor != None):
-                cursor.close()
-            if (conn != None):
-                conn.rollback()
-                conn.close()
-            else:
-                print("Failed to read data")
-    else:
-        return Response("Sign up failed",
-                        mimetype="text/plain", 
-                        status=400)
-
-def find_user():
+def user():
     if (request.method == 'GET'):
         conn = None
         cursor = None
@@ -120,13 +66,58 @@ def find_user():
                 conn.close()
             else:
                 print("Failed to read data")
-    else:
-        return Response("Could not load info",
-                        mimetype="text/plain",
-                        status=400)
+  
+    elif (request.method == 'POST'):
+        conn = None
+        cursor = None
+        username = request.json.get("username"),
+        password = request.json.get("password")
+        bio = request.json.get("bio")
+        email = request.json.get("email")
+        birthdate = request.json.get("birthdate")
 
-def edit_user():
-    if (request.method == 'PATCH'):
+        try:
+            (conn, cursor) = dbConnect()
+            cursor.execute("INSERT INTO user(username, password, bio, email, birthdate) VALUES(?,?,?,?,?)", [username, password, bio, email, birthdate])
+            conn.commit()
+            login_token = secrets.token_urlsafe(20)
+            cursor.execute("INSERT INTO user_session(user_id, loginToken) VALUES=?,?",[userid, login_token])
+            conn.commit()
+            newUser = {
+                "userId": userid,
+                "username": username,
+                "password": password,
+                "bio": bio,
+                "email": email,
+                "birthdate": birthdate,
+                "loginToken": login_token
+            }
+            return Response(json.dumps(newUser),
+                        mimetype="application/json",
+                        status=200)
+        except ValueError as error:
+            print("Error" +str(error))
+        except mariadb.DataError:
+            print("something went wrong with your data")
+        except mariadb.OperationalError:
+            print("opertational error on the connection")
+        except mariadb.ProgrammingError:
+            print("apparently, you don't know how to code")
+        except mariadb.IntegrityError:
+            print("Error with DB integrity. most likelu constraint failure")
+        except:
+            print("Something went wrong")
+
+        finally:
+            if (cursor != None):
+                cursor.close()
+            if (conn != None):
+                conn.rollback()
+                conn.close()
+            else:
+                print("Failed to read data")
+   
+    elif (request.method == 'PATCH'):
         conn = None
         cursor = None
         login_token = request.json.get("loginToken")
@@ -135,6 +126,7 @@ def edit_user():
         bio = request.json.get("bio")
         email = request.json.get("email")
         birthdate = request.json.get("birthdate")
+        id = request.json.get('id')
         
         try:
             (conn, cursor) = dbConnect()
@@ -172,16 +164,13 @@ def edit_user():
                 conn.close()
             else:
                 print("Failed to read data")
-    else: 
-        return("Could not update user info")
 
-
-def deleteUser(id):
-    if (request.method == 'DELETE'):
+    elif (request.method == 'DELETE'):
         conn = None
         cursor = None
         login_token = request.json.get("loginToken")
         password = request.json.get('password')
+        id = request.json.get('id')
 
         try:
             (conn, cursor) = dbConnect()
@@ -214,8 +203,10 @@ def deleteUser(id):
                 conn.close()
             else:
                 print("Failed to read data")
-    else:
-        return("Failed to delete user")
+    else: 
+        return Response("Could not delete user",
+                        mimetype="application/json",
+                        status=400)
 
    
 
