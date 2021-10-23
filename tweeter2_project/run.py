e10c9091a748b17a131a78f9042ed434274dc89d
Your Name <you@example.com>
2021-10-22 23:09:48 -0600

    ignore folder

--- tweeter2_project/run.py
+++ tweeter2_project/run.py
@@ -1,3 +1,24 @@
 from myapp import app
+import sys
 
-app.run(debug=True)
+app.run()
+
+if(len(sys.argv) > 1):
+    mode = sys.argv[1]
+    if (mode == "production"):
+        import bjoern
+        host = "0.0.0.0"
+        port = 5001
+        print("Server is running in production")
+        bjoern.run(app, host, port)
+    elif(mode == "testing"):
+        from flask_cors import CORS
+        CORS(app)
+        print("Server is running in testing mode")
+        app.run(debug=True)
+    else:
+        print("Invalid mode arguments, please choose either 'production' or 'testing'")
+        exit()
+else:
+    print("No arguement was provided")
+    exit()
